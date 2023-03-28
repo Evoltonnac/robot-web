564773bbb775e953b9e8eda0199e6af2fff88245
Evoltonnac <1173985303@qq.com>
2023-03-28 09:02:00 +0800

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const path = require('path')
 
-module.exports = nextConfig
+module.exports = {
+    webpack(config, options) {
+        config.resolve.alias['@'] = path.resolve(__dirname, '.')
+        config.module.rules.push(
+            {
+                test: /\.tsx?$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['next/babel'],
+                        },
+                    },
+                    {
+                        loader: 'ts-loader',
+                        options: {
+                            transpileOnly: true,
+                        },
+                    },
+                ],
+            })
+
+        return config
+    },
+}
