e11a8169ef1f7df8e8e11f9538a99e3506134851
Evoltonnac <1173985303@qq.com>
2023-03-29 00:18:07 +0800

    init

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,22 +1,22 @@
 import nc from 'next-connect'
 import { Configuration, OpenAIApi } from 'openai'
+import { SocksProxyAgent } from 'socks-proxy-agent'
+const proxyAgent = new SocksProxyAgent('socks5://127.0.0.1:1088')
 
 export default nc().get(async (req, res) => {
+    console.info(process.env.OPENAI_API_KEY)
     const configuration = new Configuration({
         apiKey: process.env.OPENAI_API_KEY,
     })
     const openai = new OpenAIApi(configuration)
-    
-    const response = await openai.createCompletion({
-        model: 'gpt-3.5-turbo',
-        prompt:
-        'I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with "Unknown".\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ: Where is the Valley of Kings?\nA:',
-        temperature: 0,
-        max_tokens: 100,
-        top_p: 1,
-        frequency_penalty: 0.0,
-        presence_penalty: 0.0,
-        stop: ['\n'],
+    const response = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo-0301',
+        'messages': [{'role': 'user', 'content': 'Say this is a test!'}],
+        'temperature': 0.7
+    }, {
+        httpAgent: proxyAgent,
+        httpsAgent: proxyAgent,
     })
-    res.status(200).json
+    const data = response.data.choices[0].message.content
+    res.status(200).json({data})
 })
