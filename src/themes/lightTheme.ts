24491c25f0af37ea291d751e97261cd8949d46d0
Evoltonnac <1173985303@qq.com>
2023-05-10 15:50:07 +0800

    Fix: font cannot fetch

--- src/themes/lightTheme.ts
+++ src/themes/lightTheme.ts
@@ -1,13 +1,5 @@
-import { Roboto } from 'next/font/google'
 import { createTheme } from '@mui/material/styles'
 import { blueGrey } from '@mui/material/colors'
-
-export const roboto = Roboto({
-    weight: ['300', '400', '500', '700'],
-    subsets: ['latin'],
-    display: 'swap',
-    fallback: ['Helvetica', 'Arial', 'sans-serif'],
-})
 
 // Create a theme instance.
 const theme = createTheme({
@@ -28,9 +20,6 @@ const theme = createTheme({
     },
     shape: {
         borderRadius: 8,
-    },
-    typography: {
-        fontFamily: roboto.style.fontFamily,
     },
 })
 
