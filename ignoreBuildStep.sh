edf0592390f2ec3da4e6e3dad9b239720e93ada8
Evoltonnac <1173985303@qq.com>
2023-05-16 17:32:34 +0800

    Feat: add force build env

--- ignoreBuildStep.sh
+++ ignoreBuildStep.sh
@@ -3,7 +3,7 @@
 echo "VERCEL_ENV: $VERCEL_ENV"
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if ["$VERCEL_ENV" == "production" || [ "$DOMAIN" != *"$VERCEL_GIT_COMMIT_REF"* ]]; then
+if [ "$FORCE_BUILD" == "1" || "$VERCEL_ENV" == "production" || [ "$DOMAIN" != *"$VERCEL_GIT_COMMIT_REF"* ]]; then
   exit 1
 else
   exit 0
