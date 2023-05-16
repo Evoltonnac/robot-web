22d1992b4bd8bfa5c061862be56f8d4ce9f1a757
Evoltonnac <1173985303@qq.com>
2023-05-16 19:00:21 +0800

    Fix: ignore build step bash

--- ignoreBuildStep.sh
+++ ignoreBuildStep.sh
@@ -3,7 +3,10 @@
 echo "VERCEL_ENV: $VERCEL_ENV"
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [ "$FORCE_BUILD" == "1" || "$VERCEL_ENV" == "production" || [ "$DOMAIN" != *"$VERCEL_GIT_COMMIT_REF"* ]]; then
+# 1.FORCE_BUILD equals 1
+# 2.in production env
+# 3.branch name has coincident DOMAIN env
+if [ "$FORCE_BUILD" == "1" ] || [ "$VERCEL_ENV" == "production" ] || [[ "$DOMAIN" != *"$VERCEL_GIT_COMMIT_REF"* ]]; then
   exit 1
 else
   exit 0
