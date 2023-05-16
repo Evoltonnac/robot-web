e79acb99f195ae112e6272383a2dc117c4909e64
Evoltonnac <1173985303@qq.com>
2023-05-16 17:20:27 +0800

    Feat: vercel ignore build step bash

---
+++ ignoreBuildStep.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "VERCEL_ENV: $VERCEL_ENV"
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+if ["$VERCEL_ENV" == "production" || [ "$DOMAIN" != *"$VERCEL_GIT_COMMIT_REF"* ]]; then
+  exit 1
+else
+  exit 0
+fi
