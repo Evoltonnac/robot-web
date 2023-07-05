2923f1ac0a283b19c083ca473396e3dbafa42ee2
Evoltonnac <1173985303@qq.com>
2023-07-05 15:18:04 +0800

    🤡 test on vercel

--- pages/test.tsx
+++ pages/test.tsx
@@ -34,9 +34,6 @@ export default function StreamViewer() {
 
     return (
         <div style={{ margin: '4rem' }}>
-            <a href="/" style={{ fontSize: '16px', marginBottom: '20px', color: 'lightblue', textDecoration: 'underline' }}>
-                To Fetch Demo 👉
-            </a>
             <h1 style={{ fontSize: '24px', fontWeight: 'bold', marginBottom: '16px' }}>
                 Demo of Abort Signal not working with Edge (with EventSource)
             </h1>
