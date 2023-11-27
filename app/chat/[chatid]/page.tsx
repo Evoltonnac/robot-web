e322450beb447db0146b9417ba9afdba64f55228
Xminghua <1173985303@qq.com>
2023-11-27 21:00:30 +0800

    💄 fix mobile chat page style

--- app/chat/[chatid]/page.tsx
+++ app/chat/[chatid]/page.tsx
@@ -1,18 +1,16 @@
 import Head from 'next/head'
 import ChatBot from '@/components/chat/ChatBot'
 import { Container } from '@mui/material'
-import { useSearchParams } from 'next/navigation'
 
-export default function ChatById() {
-    const searchParams = useSearchParams()
-    const chatid = searchParams.get('chatid')
-
+export default function ChatById({ params }: { params: { chatid: string } }) {
     return (
         <>
             <Head>
                 <title>Robot ♂ Chat</title>
             </Head>
-            <Container sx={{ height: '100vh', padding: 0 }}>{chatid ? <ChatBot chatid={chatid}></ChatBot> : null}</Container>
+            <Container sx={{ height: '100vh', padding: 0 }}>
+                {params.chatid ? <ChatBot chatid={params.chatid} isFullScreen></ChatBot> : null}
+            </Container>
         </>
     )
 }
